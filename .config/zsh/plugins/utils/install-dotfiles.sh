c22d88b963ce137b95c05327f2ae1afcb1fa6f92
Mackie Burgess <mackie.burgess@gmail.com>
2022-12-30 00:47:49 +0000

    dotfiles install script

---
+++ .config/zsh/plugins/utils/install-dotfiles.sh
@@ -0,0 +1,18 @@
+git clone --bare git@github.com:mackieburgess/dotfiles.git $HOME/.config/dotfiles
+
+function dotfiles {
+   /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
+}
+
+mkdir -p $HOME/.cache/config-backup
+dotfiles checkout
+
+if [ $? = 0 ]; then
+  echo "Checked out config.";
+  else
+    echo "Backing up pre-existing dotfiles.";
+    dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} ~/.cache/config-backup/{}
+fi;
+
+dotfiles checkout
+
